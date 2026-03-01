27ec33520bcdf674ba723e3875e7723256ded30c
AthirahWahyu <athirahsasongko07@gmail.com>
2026-03-01 09:35:56 +0700

    Jobsheet 3 "Array of Object" Percobaan 3

---
+++ Praktikum03/Matakuliah3.java
@@ -0,0 +1,15 @@
+package Praktikum03;
+
+public class Matakuliah3 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah3(String kode, String nama, int sks, int jumlahJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
